212af7a7f088b4f1d87a8eb4c7d3d54f976e26ab
ArnMips <arzhevitin.bk@gmail.com>
2017-04-11 22:06:23 +0300

    Edit the design

--- code/Object.h
+++ code/Object.h
@@ -5,8 +5,8 @@
 
 class Object{
 public:
-    //PropertyFields: first - key | second - value
-    typedef std::vector<std::pair<std::string, std::string> > PropertyFields;
+	//PropertyFields: first - key | second - value
+	typedef std::vector<std::pair<std::string, std::string> > PropertyFields;
 
 	void setType(std::string& objectType);
 	void setName(std::string& objectName);
@@ -16,11 +16,11 @@ public:
 	std::string getType() const;
 	std::string getName() const;
 	std::vector<std::pair<double, double>> getCoordinates() const;
-    PropertyFields getDrawProperty() const;
+	PropertyFields getDrawProperty() const;
 
 private:
 	std::vector<std::pair<double, double>> coordinates_;
 	std::string name_;
 	std::string type_;
-    PropertyFields drawProperty_;
+	PropertyFields drawProperty_;
 };
